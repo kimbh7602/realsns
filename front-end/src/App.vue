5b92bc3bd75ee6a37f2110df8a26c6c886fd0062
Y <ysj4161@naver.com>
2020-02-03 17:46:36 +0900

    temp

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -5,19 +5,31 @@
     <Sideex />
     <main class="main-content">
       <!-- 공지 -->
-      <div class="all-scroll pos-relative mt-50 col-12" style="height:50px">
-        <div class="swiper-scrollbar"></div>
-        <div class="swiper-container oflow-visible" style=" text-align:center;" data-slide-effect="flip" data-autoheight="false" 
-                                data-swiper-speed="5000" data-swiper-margin="25" data-swiper-slides-per-view="1"  data-swiper-wheel-control="true"
-                                data-swiper-breakpoints="true" data-scrollbar="true" data-swiper-loop="true" data-swiper-direction="vertical" 
-                                data-swiper-autoplay="true" data-swpr-responsive="[1, 2, 1, 2]">
-            <div class="swiper-wrapper" style="height:50px">
-                    <div class="flux swiper-slide" v-for="no in noti" :key="no.index">
-                      {{no}}
-                    </div>
+      <div class="top-noti" style="background-color:black; width:100%">
+        <div class="text-white offset-4 col-1" style="text-align:right; display:inline-block;"><span class="icon-notifications_active neon"></span></div>
+        <div class="all-scroll col-7 mt-50" style="width:100%; display:inline-block;">
+          <div class="swiper-scrollbar"></div>
+          <div class="swiper-container oflow-visible" style="padding-top:10px; background-color:black; height:50px;" data-slide-effect="flip" data-autoheight="false" 
+                                  data-swiper-speed="5000" data-swiper-margin="25" data-swiper-slides-per-view="1"  data-swiper-wheel-control="true"
+                                  data-swiper-breakpoints="true" data-scrollbar="true" data-swiper-loop="true" data-swiper-direction="vertical" 
+                                  data-swiper-autoplay="true" data-swpr-responsive="[1, 2, 1, 2]">
+              <div class="swiper-wrapper" style="height:10px;">
+                <div class="elip text-white swiper-slide" style="background-color:black;" v-for="no in noti" :key="no.index">
+                  {{no}}
+                </div>
+              </div>
             </div>
         </div>
-    </div>
+      </div>
+
+      <!-- 공지 2 -->
+      <!-- <MARQUEE onmouseover=this.stop() style="WIDTH: 100%; HEIGHT: 10px" onmouseout=this.start() scrollAmount=1 scrollmount=10 direction=up>
+        <div style="PADDING-RIGHT: 0px; OVERFLOW-Y: auto; PADDING-LEFT: 0px; OVERFLOW-X: hidden; PADDING-BOTTOM: 3px; WIDTH: 100%; PADDING-TOP: 3px">
+          <div class="neon" style="display:inline-block; background-color:black;" v-for="no in noti" :key="no.index">
+                {{no}}
+          </div>
+        </div>
+      </MARQUEE> -->
       <router-view :key="$route.fullPath"></router-view>
       <Footer />
     </main>
@@ -51,7 +63,7 @@ export default {
   name: 'app',
   data(){
     return{
-      noti:["aaaaaaaaa","bbbbbbbbbbbbbbb","cccccccccccccccc","dddddddddddddddd","eeeeeeeeeeeee","ffffffffffffff","ggggggggggggg","hhhhhhhhhhhhhhhh","iiiiiiiiiiiiiiiii","aaaaaaaaa"],
+      noti:["!!!!!","bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb","cccccccccccccccc","dddddddddddddddd","eeeeeeeeeeeee","ffffffffffffff","ggggggggggggg","hhhhhhhhhhhhhhhh","iiiiiiiiiiiiiiiii","aaaaaaaaa"],
     }
   },
   components: {
@@ -67,3 +79,13 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.elip{
+  text-overflow:ellipsis;
+  white-space:nowrap;
+  word-wrap:normal;
+  width:180px;
+  overflow:hidden;
+}
+</style>
