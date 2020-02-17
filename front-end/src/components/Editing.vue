acd2387e651299b94125e196f4672ef613d1596c
김보현 <kimbh7602@gmail.com>
2020-02-17 15:51:35 +0900

    node_modules

--- front-end/src/components/Editing.vue
+++ front-end/src/components/Editing.vue
@@ -39,9 +39,9 @@
 </template>
 
 <script>
-    import '/home/ubuntu/realsns/node_modules/cropperjs/dist/cropper.css';
-    import '/home/ubuntu/realsns/node_modules/cropperjs/dist/cropper.min.css';
-    import Cropper from "/home/ubuntu/realsns/node_modules/cropperjs";
+    import 'cropperjs/dist/cropper.css';
+    import 'cropperjs/dist/cropper.min.css';
+    import Cropper from "cropperjs";
     export default {
         name: "editing",
         props: ["imgs","prevpage","oldpw","items","tags"],
