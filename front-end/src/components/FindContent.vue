8aa2be94b6d18b1c68476f3996173c29ddd01fe8
Y <ysj4161@naver.com>
2020-02-19 11:48:12 +0900

    t

--- front-end/src/components/FindContent.vue
+++ front-end/src/components/FindContent.vue
@@ -16,7 +16,7 @@
                     <label class="text-white">Location Search</label> 
                     <input v-model="searchKeyword" type="text" class="form-control">
                     <br>
-                    <!-- <input data-toggle="modal" data-target="#mapModal" @click="searchLocation" type="button" value="Search" class="btn btn-outline-light btn-block"> -->
+                    <input data-toggle="modal" @click="searchLocation" type="button" value="Search" class="btn btn-outline-light btn-block">
                     </div>
                 </div>
                 <div v-if="!isLocation&&!isLocationSelect">
@@ -331,6 +331,7 @@ export default {
                                         text: addr
                                     }
                                 })
+                                document.getElementById("mapModal").click();
                             })
                             .catch((error)=>{
                               alert(error)
