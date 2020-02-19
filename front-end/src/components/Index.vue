69546b595007df63e880a49d50cd22a4f0b98189
Y <ysj4161@naver.com>
2020-02-20 01:55:50 +0900

    t

--- front-end/src/components/Index.vue
+++ front-end/src/components/Index.vue
@@ -233,7 +233,7 @@ export default {
                     if(res.data.resmsg=='해시태그 포함 게시물 리스트 출력 성공'){
                       for(var idx9=0; idx9<res.data.resValue.length; idx9++){
                         this.dislikeList.push(res.data.resValue[idx9].content_id);
-                        window.console.log(this.dislikeList);
+                        window.console.log(res.data.resValue[idx9].content_id);
                       }
                     }
                   })
