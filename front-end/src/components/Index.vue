c3eedcbaa1a93e8a400a5db635306ab997c9dde0
Y <ysj4161@naver.com>
2020-02-20 01:50:48 +0900

    t

--- front-end/src/components/Index.vue
+++ front-end/src/components/Index.vue
@@ -231,6 +231,7 @@ export default {
                   .get('content/contentListHashtag/'+dis)
                   .then((res)=>{
                     if(res.data.resValue.length>0){
+                      window.console.log(res.data.resValue);
                       for(var idx9=0; idx9<res.data.resValue.length; idx9++){
                         this.dislikeList.push(res.data.resValue[idx9].content_id);
                       }
@@ -246,7 +247,7 @@ export default {
           this.errored = true
         })
         .finally(()=>{
-          window.console.log(this.dislikeList);
+          // window.console.log(this.dislikeList);
         })
     },
     // matchingDislike(){
