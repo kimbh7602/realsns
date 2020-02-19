ff6ab7bb715e1b2d48b3b9a34ee2ca19fd8b6d41
Y <ysj4161@naver.com>
2020-02-19 15:54:36 +0900

    t

--- front-end/src/components/Category.vue
+++ front-end/src/components/Category.vue
@@ -335,6 +335,7 @@ export default {
           this.contentErrorMsg = ""
           if (res.data.resmsg == "개인 게시물 리스트 출력 성공") {
             for (var i = 0; i < res.data.resValue.length; i++) {
+              res.data.resValue[i].content_val = res.data.resValue[i].content_val.replace(/\n/g, "<br />");
               for (var j = 0; j < this.contentIds.length; j++) {
                 if (res.data.resValue[i].content_id == this.contentIds[j].con_id) {
                   res.data.resValue[i].user_like = true
@@ -360,6 +361,7 @@ export default {
           this.contentErrorMsg = ""
           if (res.data.resmsg == "스크랩목록성공") {
             for (var i = 0; i < res.data.resvalue.length; i++) {
+              res.data.resValue[i].content_val = res.data.resValue[i].content_val.replace(/\n/g, "<br />");
               for (var j = 0; j < this.contentIds.length; j++) {
                 if (res.data.resvalue[i].content_id == this.contentIds[j].con_id) {
                   res.data.resvalue[i].user_like = true
@@ -383,6 +385,7 @@ export default {
         if (res.data.resvalue.length > 0) {
           if (res.data.resmsg == "스크랩목록성공") {
             for (var i = 0; i < res.data.resvalue.length; i++) {
+              res.data.resValue[i].content_val = res.data.resValue[i].content_val.replace(/\n/g, "<br />");
               for (var j = 0; j < this.contentIds.length; j++) {
                 if (res.data.resvalue[i].content_id == this.contentIds[j].con_id) {
                   res.data.resvalue[i].user_like = true
