dd67eb766199312afaa0e935c9d1d4917533d4fc
Y <ysj4161@naver.com>
2020-02-20 01:56:34 +0900

    t

--- front-end/src/components/Index.vue
+++ front-end/src/components/Index.vue
@@ -231,6 +231,7 @@ export default {
                   .get('content/contentListHashtag/'+dis)
                   .then((res)=>{
                     if(res.data.resmsg=='해시태그 포함 게시물 리스트 출력 성공'){
+                      window.console.log(res.data.resValue.length)
                       for(var idx9=0; idx9<res.data.resValue.length; idx9++){
                         this.dislikeList.push(res.data.resValue[idx9].content_id);
                         window.console.log(res.data.resValue[idx9].content_id);
